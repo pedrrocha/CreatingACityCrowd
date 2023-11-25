f7c41ad647f2c5b180572fb51f9d4413944189b1
pedrrocha <126675591+pedrrocha@users.noreply.github.com>
2023-11-25 15:57:49 +0000

    Implementaçao do cenario final/movimentaçao

--- Assets/AIControl.cs
+++ Assets/AIControl.cs
@@ -8,19 +8,29 @@ public class AIControl : MonoBehaviour {
     GameObject[] goalLocations;
     NavMeshAgent agent;
     Animator anim;
-    void Start()
-    {
 
-        agent = this.GetComponent<NavMeshAgent>();
+    void Start() {
+
+        agent = GetComponent<NavMeshAgent>();
         goalLocations = GameObject.FindGameObjectsWithTag("goal");
-        int i = Random.Range(0, goalLocations.length);
+        int i = Random.Range(0, goalLocations.Length);
         agent.SetDestination(goalLocations[i].transform.position);
-        anim = this.goalLocations<Animator>();
+        anim = this.GetComponent<Animator>();
         anim.SetTrigger("isWalking");
+        anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
+        float sm = Random.Range(0.5f, 2.0f);
+        anim.SetFloat("speedMult", sm);
+        agent.speed *= sm;
+
     }
 
 
     void Update() {
 
+        if (agent.remainingDistance < 1.0f) {
+
+            int i = Random.Range(0, goalLocations.Length);
+            agent.SetDestination(goalLocations[i].transform.position);
+        }
     }
 }
